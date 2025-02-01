9e1e1ccb44f3b326c4eb04027427d218b0875349
Pritam Adhikari <133486121+pritam890@users.noreply.github.com>
2025-02-01 22:57:06 +0530

    Update server.js

--- server/server.js
+++ server/server.js
@@ -12,7 +12,7 @@ const app = express()
 
 app.use(express.json())
 app.use(cors({
-  origin: '',
+  origin: 'https://imagify-frontend-puce.vercel.app',
   credentials: true,
   methods: ['GET', 'POST', 'PUT', 'DELETE'],
   allowedHeaders: ['Content-Type', 'Authorization']
