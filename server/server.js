5b4f9b90371bd26ff1b290fadfcf77feef97c138
Pritam Adhikari <133486121+pritam890@users.noreply.github.com>
2025-02-01 22:53:00 +0530

    Update server.js

--- server/server.js
+++ server/server.js
@@ -11,7 +11,12 @@ const PORT = process.env.PORT || 4000
 const app = express()
 
 app.use(express.json())
-app.use(cors())
+app.use(cors({
+  origin: '',
+  credentials: true,
+  methods: ['GET', 'POST', 'PUT', 'DELETE'],
+  allowedHeaders: ['Content-Type', 'Authorization']
+}));
 await connectDB()
 
 app.use('/api/user', userRouter)
